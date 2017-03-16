3630e99bc9e8c5a2e0e1afdcc7ed1f43b95fc7b2
teesitk <xx@xx.xx>
2017-03-16 11:36:23 +0700

    updated

--- src/js/components/About.jsx
+++ src/js/components/About.jsx
@@ -15,7 +15,7 @@ class About extends React.Component {
     render() {
         return (
             <div className="card">
-			  <img className="card-img-top" src="http://media.thaitv3.com/store/v4/drama/2017/03/0971920567.jpg" alt="Card image cap"/>
+			  <img className="card-img-top" src="http://seanamarasinghe.com/wp-content/uploads/2015/09/react-es61-1050x360.jpg" alt="Card image cap"/>
 			  <div className="card-block">
 			    <p className="card-text"> The term isomorphism literally means sameness (iso) of form (morphism). In Gestalt psychology, Isomorphism is the idea that perception and the underlying physiological representation are similar because of related Gestalt qualities. Isomorphism refers to a correspondence between a stimulus array and the brain state created by that stimulus, and is based on the idea that the objective brain processes underlying and correlated with particular phenomenological experiences functionally have the same form and structure as those subjective experiences</p>
 			  </div>
