37c1b14bd1b1a10e2c0cd287c0c5e8c4268cd9b2
teesit <teesitk@becicorp.com>
2018-06-14 16:48:25 +0700

    npm install && webpack

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
  
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+import createBrowserHistory from 'history/createBrowserHistory';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App.jsx'
  
